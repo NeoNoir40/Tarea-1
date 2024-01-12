a3f6f1bebde03d5d3c08921c0a6d83c44d59a657
NeoNoir40 <neroxartz@hotmail.com>
2024-01-12 09:21:43 -0500

    first commit

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('Dano es gei')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text> </Text>
       <StatusBar style="auto" />
     </View>
   );
